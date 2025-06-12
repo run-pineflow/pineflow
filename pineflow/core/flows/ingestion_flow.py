8fe214834053bbe58a61a67f42e617c0468aedf4
Leonardo Furnielis <leonardofurnielis@outlook.com>
2025-06-11 23:16:23 -0300

    feat: add new param for de-dup stage (pre and post)

--- pineflow/core/flows/ingestion_flow.py
+++ pineflow/core/flows/ingestion_flow.py
@@ -10,11 +10,13 @@ class IngestionFlow():
     """An ingestion flow for processing and storing data.
 
     Args:
-        transformers (List[TransformerComponent]): A list of transformers to apply to the data.
-        dedup_strategy (str): Document de-dup strategy. 
+        transformers (List[TransformerComponent]): A list of transformer components applied to the input documents.
+        dedup_stage (str): The stage at which deduplication is applied (before or after transformation). 
+                        Currently supports "pre_transform" and "post_transform".
+        dedup_strategy (str): The strategy used for handling duplicates. 
                             Currently supports "duplicate_only", "duplicate_and_delete", and "deduplicate_off".
-        readers (BaseReader, optional): Reader to use for ingesting data.
-        vector_store (BaseVectorStore, optional): Vector store to use for storing the data.
+        readers (BaseReader, optional): List of readers for loading or fetching documents.
+        vector_store (BaseVectorStore, optional): Vector store for saving processed documents
 
     Example:
         .. code-block:: python
@@ -33,12 +35,14 @@ class IngestionFlow():
 
     def __init__(self, 
                  transformers: List[TransformerComponent],
-                 dedup_strategy: Literal["duplicate_only","duplicate_and_delete","deduplicate_off"] = "duplicate_only",
-                 readers: List[BaseReader] = None,
-                 vector_store: BaseVectorStore = None
+                 dedup_stage: Literal["pre_transform", "post_transform"] = "pre_transform",
+                 dedup_strategy: Literal["duplicate_only", "duplicate_and_delete", "deduplicate_off"] = "duplicate_only",
+                 readers: Optional[List[BaseReader]] = None,
+                 vector_store: Optional[BaseVectorStore] = None
                  ) -> None:
         
         self.readers = readers
+        self.dedup_stage = dedup_stage
         self.dedup_strategy = dedup_strategy
         self.transformers = transformers
         self.vector_store = vector_store
@@ -57,14 +61,18 @@ class IngestionFlow():
     
     def _handle_duplicates(self, documents) -> List[Document]:
         ids, existing_hashes, existing_ref_hashes = self.vector_store.get_all_document_hashes()
-        # Fallback to document own hash if `ref_doc_hash` is missing (for de-duplication)
-        hashes_fallback = [existing_ref_hashes[i] if existing_ref_hashes[i] is not None else existing_hashes[i] 
-                                for i in range(len(existing_ref_hashes))]
-        
+        if self.dedup_stage == "pre_transform":
+            # Fallback to document own hash if `ref_doc_hash` (parent level) is missing for de-duplication
+            hashes_fallback = [existing_ref_hashes[i] if existing_ref_hashes[i] is not None else existing_hashes[i] 
+                                        for i in range(len(existing_ref_hashes))]
+        elif self.dedup_stage == "post_transform":
+            # Use own document hash (chunks level) for de-duplication
+            hashes_fallback = existing_hashes
+            
         current_hashes = []
         current_unique_hashes = []
         dedup_documents_to_run = []
-        
+
         for doc in documents:
             current_hashes.append(doc.hash)
 
@@ -80,14 +88,16 @@ class IngestionFlow():
  
             if self.vector_store is not None:
                 self.vector_store.delete_documents(ids_to_remove)
-        
+
         return dedup_documents_to_run
                 
     def _run_transformers(self, documents: List[Document], transformers: TransformerComponent) -> List[Document]:
-        for transform in transformers:
-            documents = transform(documents)
+        _documents = documents.copy()
         
-        return documents    
+        for transform in transformers:
+            _documents = transform(_documents)
+        
+        return _documents    
     
     def run(self, documents: List[Document]=None) -> List[Document]:
         """Run an ingestion flow.
@@ -102,17 +112,27 @@ class IngestionFlow():
         """
         input_documents = self._read_documents(documents)
         
-        if self.vector_store is not None and self.dedup_strategy != "deduplicate_off":
+        # Apply pre-transform de-dup (parent level)
+        if (self.vector_store is not None and 
+            self.dedup_strategy != "deduplicate_off" and
+            self.dedup_stage == "pre_transform"):
+            
             documents_to_run = self._handle_duplicates(input_documents)
         else:
             documents_to_run = input_documents
         
         if documents_to_run:
             documents = self._run_transformers(documents_to_run, self.transformers)
-        
+
             documents = documents or []
             
-            if self.vector_store is not None:
+            # Apply post-transform de-dup (chunk level)
+            if (self.vector_store is not None and
+            self.dedup_strategy != "deduplicate_off" and
+            self.dedup_stage == "post_transform"):
+                documents = self._handle_duplicates(documents)
+            
+            if self.vector_store is not None and documents:
                 self.vector_store.add_documents(documents)
         
         return documents
