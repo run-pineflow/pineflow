f7b99e4623ece141ca665a15e1164adacffb7bce
Leonardo Furnielis <leonardofurnielis@outlook.com>
2025-06-17 23:52:22 -0300

    style: lint

--- pineflow/readers/__init__.py
+++ pineflow/readers/__init__.py
@@ -1,9 +1,9 @@
 from pineflow.core.readers.directory import DirectoryReader
 from pineflow.readers.docx import DocxReader
 from pineflow.readers.html import HTMLReader
+from pineflow.readers.ibm_cos import IBMCOSReader
 from pineflow.readers.json import JSONReader
 from pineflow.readers.pdf import PDFReader
-from pineflow.readers.ibm_cos import IBMCOSReader
 from pineflow.readers.watson_discovery import WatsonDiscoveryReader
 
 __all__ = [
