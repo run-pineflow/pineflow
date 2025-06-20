1280699bd568b971f717081c977c357d074769a1
Leonardo Furnielis <leonardofurnielis@outlook.com>
2025-06-20 19:00:38 -0300

    refactor: move file readers to pineflow-extensions

---
+++ pineflow-extensions/readers/file/pineflow/readers/file/__init__.py
@@ -0,0 +1,11 @@
+from pineflow.readers.file.docx import DocxReader
+from pineflow.readers.file.html import HTMLReader
+from pineflow.readers.file.json import JSONReader
+from pineflow.readers.file.pdf import PDFReader
+
+__all__ = [
+    "DocxReader",
+    "HTMLReader",
+    "JSONReader",
+    "PDFReader",
+]
